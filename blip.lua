e92b02501eeee511861c94e0da00c7b32f8dbbd3
paw <pawsen@gmail.com>
2014-03-16 01:17:42 +0100

    Updated readme

--- blip.lua
+++ blip.lua
@@ -1,4 +1,6 @@
 #!/usr/bin/env lem
+-- -*- coding: utf-8 -*-
+
 --
 -- This file is part of blipserver.
 -- Copyright 2011 Emil Renner Berthing
@@ -30,52 +32,53 @@ local tonumber = tonumber
 
 local get_blip, put_blip
 do
-	local thisthread, suspend, resume
-		= utils.thisthread, utils.suspend, utils.resume
-	local queue, n = {}, 0
+   local thisthread, suspend, resume
+      = utils.thisthread, utils.suspend, utils.resume
+   local queue, n = {}, 0
 
-	function get_blip()
-		n = n + 1;
-		queue[n] = thisthread()
+   function get_blip()
+      n = n + 1;
+      queue[n] = thisthread()
 
-		return suspend()
-	end
+      return suspend()
+   end
 
-	function put_blip(stamp, ms)
-		print(stamp, ms, n)
-		for i = 1, n do
-			resume(queue[i], stamp, ms)
-			queue[i] = nil
-		end
+   function put_blip(stamp, ms)
+      print(stamp, ms, n)
+      for i = 1, n do
+         resume(queue[i], stamp, ms)
+         queue[i] = nil
+      end
 
-		n = 0
-	end
+      n = 0
+   end
 end
 
 utils.spawn(function()
-	local serial = assert(io.open('/dev/serial/blipduino', 'r'))
-	local db = assert(postgres.connect('user=powermeter dbname=powermeter'))
-	local now = utils.now
-	assert(db:prepare('put', 'INSERT INTO readings VALUES ($1, $2)'))
+      local serial = assert(io.open('/dev/arduino', 'r'))
+      -- local serial = assert(io.open('/dev/ttyUSB0', 'r'))
+      local db = assert(postgres.connect('user=powermeter dbname=powermeter'))
+      local now = utils.now
+      assert(db:prepare('put', 'INSERT INTO readings VALUES ($1, $2)'))
 
-	-- discard first two readings
-	assert(serial:read('*l'))
-	assert(serial:read('*l'))
+      -- discard first two readings
+      assert(serial:read('*l'))
+      assert(serial:read('*l'))
 
-	while true do
-		local ms = assert(serial:read('*l'))
-		local stamp = format('%0.f', now() * 1000)
+      while true do
+         local ms = assert(serial:read('*l'))
+         local stamp = format('%0.f', now() * 1000)
 
-		put_blip(stamp, ms)
-		assert(db:run('put', stamp, ms))
-	end
+         put_blip(stamp, ms)
+         assert(db:run('put', stamp, ms))
+      end
 end)
 
 local function sendfile(content, path)
-	return function(req, res)
-		res.headers['Content-Type'] = content
-		res.file = path
-	end
+   return function(req, res)
+      res.headers['Content-Type'] = content
+      res.file = path
+   end
 end
 
 hathaway.import()
@@ -88,41 +91,41 @@ GET('/excanvas.js',    sendfile('text/javascript; charset=UTF-8', 'excanvas.js')
 GET('/favicon.ico',    sendfile('image/x-icon',                   'favicon.ico'))
 
 local function apiheaders(headers)
-	headers['Content-Type'] = 'text/javascript; charset=UTF-8'
-	headers['Cache-Control'] = 'max-age=0, must-revalidate'
-	headers['Access-Control-Allow-Origin'] = '*'
-	headers['Access-Control-Allow-Methods'] = 'GET'
-	headers['Access-Control-Allow-Headers'] = 'origin, x-requested-with, accept'
-	headers['Access-Control-Max-Age'] = '60'
+   headers['Content-Type'] = 'text/javascript; charset=UTF-8'
+   headers['Cache-Control'] = 'max-age=0, must-revalidate'
+   headers['Access-Control-Allow-Origin'] = '*'
+   headers['Access-Control-Allow-Methods'] = 'GET'
+   headers['Access-Control-Allow-Headers'] = 'origin, x-requested-with, accept'
+   headers['Access-Control-Max-Age'] = '60'
 end
 
 local function apioptions(req, res)
-	apiheaders(res.headers)
-	res.status = 200
+   apiheaders(res.headers)
+   res.status = 200
 end
 
 OPTIONS('/blip', apioptions)
 GET('/blip', function(req, res)
-	apiheaders(res.headers)
+       apiheaders(res.headers)
 
-	local stamp, ms = get_blip()
-	res:add('[%s,%s]', stamp, ms)
+       local stamp, ms = get_blip()
+       res:add('[%s,%s]', stamp, ms)
 end)
 
 local function add_json(res, values)
-	res:add('[')
+   res:add('[')
 
-	local n = #values
-	if n > 0 then
-		for i = 1, n-1 do
-			local point = values[i]
-			res:add('[%s,%s],', point[1], point[2])
-		end
-		local point = values[n]
-		res:add('[%s,%s]', point[1], point[2])
-	end
+   local n = #values
+   if n > 0 then
+      for i = 1, n-1 do
+         local point = values[i]
+         res:add('[%s,%s],', point[1], point[2])
+      end
+      local point = values[n]
+      res:add('[%s,%s]', point[1], point[2])
+   end
 
-	res:add(']')
+   res:add(']')
 end
 
 local db = assert(qpostgres.connect('user=powermeter dbname=powermeter'))
@@ -131,35 +134,35 @@ assert(db:prepare('last', 'SELECT stamp, ms FROM readings ORDER BY stamp DESC LI
 
 OPTIONS('/last', apioptions)
 GET('/last', function(req, res)
-	apiheaders(res.headers)
+       apiheaders(res.headers)
 
-	local point = assert(db:run('last'))[1]
+       local point = assert(db:run('last'))[1]
 
-	res:add('[%s,%s]', point[1], point[2])
+       res:add('[%s,%s]', point[1], point[2])
 end)
 
 OPTIONSM('^/since/(%d+)$', apioptions)
 GETM('^/since/(%d+)$', function(req, res, since)
-	if #since > 15 then
-		httpserv.bad_request(req, res)
-		return
-	end
-	apiheaders(res.headers)
-	add_json(res, assert(db:run('get', since)))
+        if #since > 15 then
+           httpserv.bad_request(req, res)
+           return
+        end
+        apiheaders(res.headers)
+        add_json(res, assert(db:run('get', since)))
 end)
 
 OPTIONSM('^/last/(%d+)$', apioptions)
 GETM('^/last/(%d+)$', function(req, res, ms)
-	if #ms > 15 then
-		httpserv.bad_request(req, res)
-		return
-	end
-	apiheaders(res.headers)
+        if #ms > 15 then
+           httpserv.bad_request(req, res)
+           return
+        end
+        apiheaders(res.headers)
 
-	local since = format('%0.f',
-		utils.now() * 1000 - tonumber(ms))
+        local since = format('%0.f',
+                             utils.now() * 1000 - tonumber(ms))
 
-	add_json(res, assert(db:run('get', since)))
+        add_json(res, assert(db:run('get', since)))
 end)
 
 hathaway.debug = print

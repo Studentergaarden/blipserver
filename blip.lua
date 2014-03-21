e8be45028740e19d0bb55ccfb1823c9c8a7969fd
paw <pawsen@gmail.com>
2014-03-21 01:13:05 +0100

    Updated the way APIs are defined: Now we avoid multiple definitions of the same APIs, eg. =lastv1= and =lastv2= is no longer dublicated code-wise

--- blip.lua
+++ blip.lua
@@ -36,6 +36,7 @@ do
       = utils.thisthread, utils.suspend, utils.resume
    local queue, n = {}, 0
 
+   -- queue thread(from http request) until put-blip is called
    function get_blipv1()
       n = n + 1;
       queue[n] = thisthread()
@@ -43,6 +44,7 @@ do
       return suspend()
    end
 
+   -- resume all http request with the recieved ms.
    function put_blipv1(stamp, ms)
       print(stamp, ms, n)
       for i = 1, n do
@@ -82,8 +84,8 @@ end
 
 utils.spawn(function()
 
-      local serial = assert(io.open('/dev/blipduino', 'r'))
-      -- local serial = assert(io.open('/dev/ttyACM0', 'r'))
+      -- local serial = assert(io.open('/dev/blipduino', 'r'))
+      local serial = assert(io.open('/dev/ttyACM0', 'r'))
       -- local serial = assert(io.open('/dev/arduino', 'r'))
       -- local serial = assert(io.open('/dev/ttyUSB0', 'r'))
       local db = assert(postgres.connect('user=powermeter dbname=powermeter'))
@@ -97,7 +99,6 @@ utils.spawn(function()
 
       while true do
          local line = assert(serial:read('*l'))
-         -- some line matching here. line:match("")
          local variable, ms = line:match("(%S+)%s+(.+)")
          local stamp = format('%0.f', now() * 1000)
 
@@ -142,23 +143,6 @@ local function apioptions(req, res)
    res.status = 200
 end
 
-OPTIONS('/blipv1', apioptions)
-GET('/blipv1', function(req, res)
-       apiheaders(res.headers)
-
-       local stamp, ms = get_blipv1()
-       res:add('[%s,%s]', stamp, ms)
-end)
-
-
-OPTIONS('/blipv2', apioptions)
-GET('/blipv2', function(req, res)
-       apiheaders(res.headers)
-
-       local stamp, ms = get_blipv2()
-       res:add('[%s,%s]', stamp, ms)
-end)
-
 local function add_json(res, values)
    res:add('[')
 
@@ -178,81 +162,84 @@ end
 local db = assert(qpostgres.connect('user=powermeter dbname=powermeter'))
 assert(db:prepare('getv1',  'SELECT stamp, ms FROM readingsv1 WHERE stamp >= $1 ORDER BY stamp LIMIT 2000'))
 assert(db:prepare('lastv1', 'SELECT stamp, ms FROM readingsv1 ORDER BY stamp DESC LIMIT 1'))
+assert(db:prepare('usagev1', 'SELECT COUNT(*) FROM readingsv1 WHERE stamp >= $1'))
 
 assert(db:prepare('getv2',  'SELECT stamp, ms FROM readingsv2 WHERE stamp >= $1 ORDER BY stamp LIMIT 2000'))
 assert(db:prepare('lastv2', 'SELECT stamp, ms FROM readingsv2 ORDER BY stamp DESC LIMIT 1'))
+assert(db:prepare('usagev2', 'SELECT COUNT(*) FROM readingsv2 WHERE stamp >= $1'))
 
 
-
-OPTIONS('/lastv1', apioptions)
-GET('/lastv1', function(req, res)
+OPTIONS('/blipv1', apioptions)
+GET('/blipv1', function(req, res)
        apiheaders(res.headers)
 
-       local point = assert(db:run('lastv1'))[1]
-
-       res:add('[%s,%s]', point[1], point[2])
+       -- get_blip queues and returns when a blip is recieved
+       local stamp, ms = get_blipv1()
+       res:add('[%s,%s]', stamp, ms)
 end)
-
-OPTIONSM('^/sincev1/(%d+)$', apioptions)
-GETM('^/sincev1/(%d+)$', function(req, res, since)
-        if #since > 15 then
-           httpserv.bad_request(req, res)
-           return
-        end
-        apiheaders(res.headers)
-        add_json(res, assert(db:run('getv1', since)))
-end)
-
-OPTIONSM('^/lastv1/(%d+)$', apioptions)
-GETM('^/lastv1/(%d+)$', function(req, res, ms)
-        if #ms > 15 then
-           httpserv.bad_request(req, res)
-           return
-        end
-        apiheaders(res.headers)
-
-        local since = format('%0.f',
-                             utils.now() * 1000 - tonumber(ms))
-
-        add_json(res, assert(db:run('getv1', since)))
-end)
-
-
-
-OPTIONS('/lastv2', apioptions)
-GET('/lastv2', function(req, res)
+OPTIONS('/blipv2', apioptions)
+GET('/blipv2', function(req, res)
        apiheaders(res.headers)
 
-       local point = assert(db:run('lastv2'))[1]
-
-       res:add('[%s,%s]', point[1], point[2])
-end)
-
-OPTIONSM('^/sincev2/(%d+)$', apioptions)
-GETM('^/sincev2/(%d+)$', function(req, res, since)
-        if #since > 15 then
-           httpserv.bad_request(req, res)
-           return
-        end
-        apiheaders(res.headers)
-        add_json(res, assert(db:run('getv2', since)))
-end)
-
-OPTIONSM('^/lastv2/(%d+)$', apioptions)
-GETM('^/lastv2/(%d+)$', function(req, res, ms)
-        if #ms > 15 then
-           httpserv.bad_request(req, res)
-           return
-        end
-        apiheaders(res.headers)
-
-        local since = format('%0.f',
-                             utils.now() * 1000 - tonumber(ms))
-
-        add_json(res, assert(db:run('getv2', since)))
+       local stamp, ms = get_blipv2()
+       res:add('[%s,%s]', stamp, ms)
 end)
 
 
+-- add API functions here
+local function defineapi(ver)
+
+   OPTIONS('/last' .. ver, apioptions)
+   GET('/last' .. ver, function(req, res)
+          apiheaders(res.headers)
+
+          local point = assert(db:run('last' .. ver))[1]
+          res:add('[%s,%s]', point[1], point[2])
+   end)
+
+   OPTIONSM('^/since'.. ver .. '/(%d+)$', apioptions)
+   GETM('^/since' .. ver .. '/(%d+)$', function(req, res, since)
+           -- if since is longer than 15 digit ...
+           if #since > 15 then
+              httpserv.bad_request(req, res)
+              return
+           end
+           apiheaders(res.headers)
+           add_json(res, assert(db:run('get' .. ver, since)))
+   end)
+
+   OPTIONSM('^/last' .. ver .. '/(%d+)$', apioptions)
+   GETM('^/last' .. ver .. '/(%d+)$', function(req, res, ms)
+           if #ms > 15 then
+              httpserv.bad_request(req, res)
+              return
+           end
+           apiheaders(res.headers)
+
+           local since = format('%0.f',
+                                utils.now() * 1000 - tonumber(ms))
+
+           add_json(res, assert(db:run('get' .. ver, since)))
+   end)
+
+   OPTIONSM('^/usage' .. ver .. '/(%d+)$', apioptions)
+   GETM('^/usage' .. ver .. '/(%d+)$', function(req, res, ms)
+           if #ms > 15 then
+              httpserv.bad_request(req, res)
+              return
+           end
+           apiheaders(res.headers)
+
+           local since = format('%0.f',
+                                utils.now() * 1000 - tonumber(ms))
+           local blips = assert(db:run('usage' .. ver, since))[1]
+           res:add('[%s]', blips[1])
+   end)
+end
+
+-- init API's defined above
+defineapi('v1')
+defineapi('v2')
 
 hathaway.debug = print
 assert(Hathaway('*', arg[1] or 8080))
